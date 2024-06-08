d271479bd4414a3d1f14a5b3d8e5ac0346e5cee7
Yoann Moreau <yoann.moreau@tutanota.com>
2024-06-08 21:32:46 +0200

    Add placeholder team command with dynamic replacement of variables in config messages

--- src/main/java/fr/ethilvan/diamondrushv2/command/Subcommand.java
+++ src/main/java/fr/ethilvan/diamondrushv2/command/Subcommand.java
@@ -1,10 +1,17 @@
 package fr.ethilvan.diamondrushv2.command;
 
 import fr.ethilvan.diamondrushv2.DiamondRush;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 public abstract class Subcommand {
 
@@ -35,9 +42,50 @@ public abstract class Subcommand {
 	protected void sendMessage(CommandSender commandSender, String messagePath) {
 		String message = diamondRush.getMessagesConfig().getString(messagePath);
 		if (message == null) {
-			commandSender.sendRichMessage("<red>Message missing from configuration: '" + messagePath + "'.");
+			missingMessage(commandSender, messagePath);
 			return;
 		}
 		commandSender.sendRichMessage(message);
+	}
+
+
+	protected void sendMessage(CommandSender commandSender, String messagePath, Map<String, String> placeholders) {
+		String message = diamondRush.getMessagesConfig().getString(messagePath);
+		if (message == null) {
+			missingMessage(commandSender, messagePath);
+			return;
+		}
+
+		TextComponent textComponent = (TextComponent) replacePlaceholders(
+				PlainTextComponentSerializer.plainText().deserialize(message),
+				placeholders
+		);
+		commandSender.sendRichMessage(textComponent.content());
+	}
+
+
+	private void missingMessage(CommandSender sender, String messagePath) {
+		sender.sendRichMessage("<red>Message missing from configuration: '" + messagePath + "'.");
+	}
+
+
+	private Component replacePlaceholders(Component component, Map<String, String> placeholders) {
+		TextComponent textComponent = (TextComponent) component;
+		Optional<String> nextKey = placeholders.keySet().stream().findFirst();
+		if (nextKey.isPresent()) {
+			String key = nextKey.get();
+			String value = placeholders.get(key);
+			if (placeholders.size() > 1) {
+				placeholders.remove(key);
+				return replacePlaceholders(
+						MiniMessage.miniMessage().deserialize(textComponent.content(), Placeholder.unparsed(key, value)),
+						placeholders
+				);
+			}
+
+			return MiniMessage.miniMessage().deserialize(textComponent.content(), Placeholder.unparsed(key, value));
+		}
+
+		return component;
 	}
 }
---
+++ src/main/java/fr/ethilvan/diamondrushv2/command/game/TeamCommand.java
@@ -0,0 +1,49 @@
+package fr.ethilvan.diamondrushv2.command.game;
+
+import fr.ethilvan.diamondrushv2.DiamondRush;
+import fr.ethilvan.diamondrushv2.command.Subcommand;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TeamCommand extends Subcommand {
+
+	public TeamCommand(DiamondRush diamondRush) {
+		super(diamondRush);
+	}
+
+
+	@Override
+	public String getName() {
+		return "team";
+	}
+
+	@Override
+	public String getSyntax() {
+		return "/diamondrush team <add|remove|modify> <teamName>";
+	}
+
+	@Override
+	public String getDescription() {
+		return "messages.commands.team.description";
+	}
+
+	@Override
+	public String getPermission() {
+		return "diamondrush.team";
+	}
+
+	@Override
+	public void perform(CommandSender sender, @NotNull String[] args) {
+		if (!sender.hasPermission(getPermission())) {
+			sendMessage(sender, "messages.commands.noPermission");
+			return;
+		}
+		Map<String, String> placeholders = new HashMap<>();
+		placeholders.put("?team-color", "gold");
+		placeholders.put("?team-name", "Orange");
+		sendMessage(sender, "messages.commands.team.addSuccess", placeholders);
+	}
+}
