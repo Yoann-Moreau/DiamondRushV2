51d3ccb32fb15bb6134bfcebf745382d1a7a5279
Yoann Moreau <yoann.moreau@tutanota.com>
2026-07-10 16:16:33 +0200

    Fix having remaining spectator after the game has ended

--- src/main/java/fr/ethilvan/diamondrushv2/listener/GamePhaseListeners.java
+++ src/main/java/fr/ethilvan/diamondrushv2/listener/GamePhaseListeners.java
@@ -382,6 +382,7 @@ public class GamePhaseListeners implements Listener {
 		if (diamondRush.getGame().getGameTimer() != null) {
 			diamondRush.getGame().getGameTimer().cancel();
 		}
+		diamondRush.getGame().getSpectators().clear();
 		diamondRush.resetScoreboard();
 		diamondRush.setGame(null);
 	}
